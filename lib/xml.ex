7ed55abcf1213e8cbeafd2e8890a66eb5c7f0dd9
hschlichter <henrik.schlichter@gmail.com>
2015-12-05 00:30:35 +0100

    First

---
+++ lib/xml.ex
@@ -0,0 +1,2 @@
+defmodule Xml do
+end
---
+++ test/xml_test.exs
@@ -0,0 +1,74 @@
+defmodule XmlTest do
+  use ExUnit.Case
+  doctest Xml
+
+  require Record
+
+  Record.defrecord :xmlElement, Record.extract(:xmlElement, from_lib: "xmerl/include/xmerl.hrl")
+  Record.defrecord :xmlText, Record.extract(:xmlText, from_lib: "xmerl/include/xmerl.hrl")
+  Record.defrecord :xmlAttribute, Record.extract(:xmlAttribute, from_lib: "xmerl/include/xmerl.hrl")
+
+  def sample_xml do
+	doc = """
+	  <html>
+		<head>
+		  <title>XML Parsing</title>
+		</head>
+		<body>
+		  <p>Neato</p>
+		  <ul>
+			<li msg="hello" msg2="fubar">First</li>
+			<li msg="world">Second</li>
+		  </ul>
+		</body>
+	  </html>
+	"""
+
+	{xml, _} = doc
+	  |> :binary.bin_to_list
+	  |> :xmerl_scan.string()
+
+	xml
+  end
+
+  test "parsing the title tag" do
+	[title_element] = :xmerl_xpath.string('/html/head/title', sample_xml)
+	[title_text] = xmlElement(title_element, :content)
+	title = xmlText(title_text, :value)
+
+	assert title == 'XML Parsing'
+  end
+
+  test "parsing the ul items" do
+	items = :xmerl_xpath.string('/html/body/ul/li', sample_xml)
+	  |> Enum.map(fn(x) ->
+		[text] = xmlElement(x, :content)
+		xmlText(text, :value)
+	  end)
+
+	assert items == ['First', 'Second']
+  end
+
+  test "parsing the attributes of the ul items" do
+	items = :xmerl_xpath.string('/html/body/ul/li', sample_xml)
+	  |> Enum.map(fn(x) ->
+		xmlElement(x, :attributes)
+		  |> Enum.map(fn(y) -> xmlAttribute(y, :value) end)
+	  end)
+	  |> Enum.concat()
+
+	assert items == ['hello', 'fubar', 'world']
+  end
+
+  test "parsing the attributes of the ul items and getting a tuple" do
+	items = :xmerl_xpath.string('/html/body/ul/li', sample_xml)
+	  |> Enum.map(fn(x) ->
+		xmlElement(x, :attributes)
+		  |> Enum.map(fn(y) -> {xmlAttribute(y, :name), xmlAttribute(y, :value)} end)
+	  end)
+	  |> Enum.concat()
+
+	# assert items == [{'msg', 'hello'}, {'msg2', 'fubar'}, {'msg', 'world'}]
+	assert items == [msg: 'hello', msg2: 'fubar', msg: 'world']
+  end
+end
